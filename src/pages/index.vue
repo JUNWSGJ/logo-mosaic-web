b2a9910409a89859cc7738fe128cd95bce09a842
keuby <knightchen@knx.com.cn>
2024-05-11 16:51:35 +0800

    feature: 添加点击事件处理

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import axios from 'axios'
-import { useMessage } from 'naive-ui'
+import { NSpin, useMessage } from 'naive-ui'
 import { useRequest } from 'vue-request'
 
 const message = useMessage()
 
-const { data } = useRequest(async () => {
+const { data, refreshAsync: refresh } = useRequest(async () => {
   const { data: respData } = await axios.get<{
     success: boolean
     errCode: number
@@ -30,20 +30,34 @@ const { data } = useRequest(async () => {
   return respData.data
 })
 
+const { runAsync: markShape, loading: marking } = useRequest(
+  async (seq: string) => {
+    await axios.post('/activity/signIn', {
+      activityId: 'ACTIVITY_001',
+      seq: seq
+    })
+    await refresh()
+  },
+  { manual: true }
+)
+
 const buildPath = (points: { x: number; y: number }[]): string => {
-  const [firstPoint] = points.slice(0, 1)
-  const restPoints = points.slice(1)
+  const [firstPoint, ...restPoints] = points
   return (
     restPoints.reduce(
-      (path, point) => `${path} L ${point.x} ${point.y}`,
-      `M ${firstPoint.x} ${firstPoint.y}`
+      (path, point) => `${path} L${point.x} ${point.y}`,
+      `M${firstPoint.x} ${firstPoint.y}`
     ) + ' Z'
   )
 }
 </script>
 
 <template>
-  <div class="inline-block" :style="`background-color: ${data?.canvasColor ?? 'black'}`">
+  <NSpin
+    :show="marking"
+    class="inline-block"
+    :style="`background-color: ${data?.canvasColor ?? 'black'}`"
+  >
     <svg
       v-if="data != null"
       :viewBox="`0 0 ${data.canvasWidth} ${data.canvasHeight}`"
@@ -52,11 +66,13 @@ const buildPath = (points: { x: number; y: number }[]): string => {
     >
       <path
         v-for="shape in data.shapes"
+        @click="markShape(shape.seq)"
         :key="shape.seq"
+        :class="{ 'cursor-pointer': !shape.marked }"
         :d="buildPath(shape.points)"
-        :data-seq="shape.seq"
-        :fill="shape.marked ? shape.color : '#ececec'"
+        stroke="#ececec80"
+        :fill="shape.marked ? shape.color : '#ececec60'"
       />
     </svg>
-  </div>
+  </NSpin>
 </template>
